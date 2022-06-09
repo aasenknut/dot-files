fd04f8f4e4cf40f42362cf9445d084f77ffd4688
KNUT <greyman@greyman>
2022-06-09 15:16:33 +0200

    Updated formatter to have all prettier formatters

--- nvim/lua/formatter_nvim.lua
+++ nvim/lua/formatter_nvim.lua
@@ -1,6 +1,17 @@
-require('formatter').setup {
-  -- Will pick formatters from defaults.
-  filetype = {
+local formatter = require('formatter')
+local prettierConfig = function()
+  return {
+    exe = "npx prettier",
+    args = {
+        "--stdin-filepath",
+        vim.fn.shellescape(vim.api.nvim_buf_get_name(0)),
+        "--single-quote"
+    },
+    stdin = true
+  }
+end
+
+local languageConfig = {
     go = {
         require('formatter.filetypes.go').gofmt,
     },
@@ -10,5 +21,49 @@ require('formatter').setup {
     json = {
         require('formatter.filetypes.json').fixjson,
     },
-  }
+-- Can add the lines that follow for more advanced config for formatting certain files.
+-- 
+--    typescript = {
+--        function()
+--            return {
+--                exe = "npx prettier",
+--                args = {
+--                    "--stdin-filepath",
+--                    vim.fn.shellescape(vim.api.nvim_buf_get_name(0)),
+--                    "--single-quote"
+--                },
+--                stdin = true
+--            }
+--        end
+--    },
 }
+
+local prettierFormatters = {
+  "css",
+  "scss",
+  "html",
+  "java",
+  "javascript",
+  "javascriptreact",
+  "typescript",
+  "typescriptreact",
+  "markdown",
+  "markdown.mdx",
+  "json",
+  "yaml",
+  "xml",
+  "svg"
+}
+for _, ft in ipairs(prettierFormatters) do
+  languageConfig[ft] = {prettierConfig}
+end
+
+formatter.setup(
+    {
+        filetype = languageConfig
+    }
+)
+
+
+
+
