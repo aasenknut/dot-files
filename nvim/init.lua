a7808eead0fe2a44d57e49aaf1794b4068c6e3e0
Knut <aasenknut@gmail.com>
2024-03-14 15:10:41 +0100

    colours

--- nvim/init.lua
+++ nvim/init.lua
@@ -24,12 +24,12 @@ require("lazy").setup(
         "airblade/vim-gitgutter",
         --colours
         {
-            "NLKNguyen/papercolor-theme",
+            "rebelot/kanagawa.nvim",
             lazy = false,    -- make sure we load this during startup if it is your main colorscheme
             priority = 1000, -- make sure to load this before all the other start plugins
             config = function()
                 -- load the colorscheme here
-                vim.cmd([[colorscheme PaperColor]])
+                vim.cmd([[colorscheme kanagawa-dragon]])
             end,
         },
         --treesitter
